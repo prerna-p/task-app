92072616935705bb4ff934907cf8b7d1a4086ec0
prerna-p <prernasocialid@gmail.com>
2020-06-25 17:30:47 -0400

    Updates

---
+++ precache-manifest.b735f0a05d173dc7b6f947e84ed49531.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "906b9b13235b7e5c0ed54c7f12317d66",
+    "url": "/task-app/index.html"
+  },
+  {
+    "revision": "672bc2dc4b96704e5f63",
+    "url": "/task-app/static/css/main.53f1dcb8.chunk.css"
+  },
+  {
+    "revision": "5ef678b8bb52373b2a0f",
+    "url": "/task-app/static/js/2.ff57e5fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/task-app/static/js/2.ff57e5fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "672bc2dc4b96704e5f63",
+    "url": "/task-app/static/js/main.bd2681d5.chunk.js"
+  },
+  {
+    "revision": "a317eb749fba53d7592c",
+    "url": "/task-app/static/js/runtime-main.14580ba2.js"
+  }
+]);
