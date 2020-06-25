fd3b4515884c0a46cb8287ea076ac8f160bba720
prerna-p <prernasocialid@gmail.com>
2020-06-25 18:46:52 -0400

    handling list when there are no tasks

--- task-manager/src/components/TaskList.js
+++ task-manager/src/components/TaskList.js
@@ -6,12 +6,12 @@ const TaskList = () => {
     const { tasks } = useContext(TaskListContext);
     return (
         <div>
-            Task List
-            <ul className="list">
-                {tasks.map((task) => {
-                    return <Task task={task} key={task.id} />;
-                })}
-            </ul>
+            {tasks.length ?
+                (<ul className="list">
+                    {tasks.map((task) => {
+                        return <Task task={task} key={task.id} />;
+                    })}
+                </ul>) : (<div className="no-tasks">No tasks yet, create one!</div>)}
         </div>
     )
 }
