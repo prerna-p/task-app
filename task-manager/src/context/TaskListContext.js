23f688af1516ae11e151055d1558f0c92e4c6fc0
prerna-p <prernasocialid@gmail.com>
2020-06-25 18:47:02 -0400

    store state in localStorage

--- task-manager/src/context/TaskListContext.js
+++ task-manager/src/context/TaskListContext.js
@@ -1,16 +1,16 @@
-import React, { createContext, useState } from 'react'
+import React, { createContext, useState, useEffect } from 'react'
 import { v4 as uuid } from 'uuid'
 
 export const TaskListContext = createContext()
 
 const TaskListContextProvider = (props) => {
-    const [tasks, setTasks] = useState([
-        { title: 'Read the book', id: 1 },
-        { title: 'Wash the car', id: 2 },
-        { title: 'Take returns to mail', id: 3 },
-    ]);
-
+    const initialState = JSON.parse(localStorage.getItem('tasks')) || [];
+    const [tasks, setTasks] = useState(initialState);
     const [editItem, setEditItem] = useState(null);
+
+    useEffect(() => {
+        localStorage.setItem('tasks', JSON.stringify(tasks))
+    }, [tasks])
 
     const addTask = (title) => {
         setTasks([...tasks, { title, id: uuid() }])
