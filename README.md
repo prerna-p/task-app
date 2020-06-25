149666a6a09f6a45be95b5c99fa00aa78f1fbf11
prerna-p <prernasocialid@gmail.com>
2020-06-25 18:29:27 -0400

    add and delete options for a task

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# task-app
+# Welcome to the repository
+
+## Checkout this task app that utilizes React Hooks and Context API
 Demo on : https://prerna-p.github.io/task-app/
--- task-manager/src/context/TaskListContext.js
+++ task-manager/src/context/TaskListContext.js
@@ -1,4 +1,5 @@
 import React, { createContext, useState } from 'react'
+import { v4 as uuid } from 'uuid'
 
 export const TaskListContext = createContext()
 
@@ -8,9 +9,44 @@ const TaskListContextProvider = (props) => {
         { title: 'Wash the car', id: 2 },
         { title: 'Take returns to mail', id: 3 },
     ]);
-    return (<TaskListContext.Provider value={{ tasks }}>
-        {props.children}
-    </TaskListContext.Provider>)
+
+    const [editItem, setEditItem] = useState(null);
+
+    const addTask = (title) => {
+        setTasks([...tasks, { title, id: uuid() }])
+    };
+
+    const deleteTask = (id) => {
+        setTasks(tasks.filter(task => task.id !== id));
+    }
+
+    const clearList = () => {
+        setTasks([]);
+    }
+
+    const findItem = (id) => {
+        const item = tasks.find(task => task.id === id);
+        setEditItem(item);
+    }
+
+    const editTask = (title, id) => {
+        const newTasks = tasks.map(task => (task.id === id ? { title, id } : task));
+        setTasks(newTasks);
+        setEditItem(null);
+    }
+    return (
+        <TaskListContext.Provider
+            value={{
+                tasks,
+                addTask,
+                deleteTask,
+                clearList,
+                editItem,
+                findItem,
+                editTask,
+            }}>
+            {props.children}
+        </TaskListContext.Provider>)
 };
 
 export default TaskListContextProvider;
